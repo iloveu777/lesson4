baf7e769fdd5a50492f836dc467c2dbea92e89d5
iloveu777 <zuxplil1337@yandex.ru>
2023-10-16 14:57:38 +0300

    zxczxc
    zxc

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string zxc; // без инициализации 
+            string zyc = null; // инициализация неопред. знач.
+            Console.WriteLine(zyc);
+            Console.ReadKey();
+        }
+    }
+}
