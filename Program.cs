a8f19800f853d2e9ec02865dfab9559e845435c1
iloveu777 <zuxplil1337@yandex.ru>
2023-10-16 15:08:20 +0300

    все 7 методов

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ConsoleApp7
 {
@@ -12,7 +13,15 @@ namespace ConsoleApp7
         {
             string zxc; // без инициализации 
             string zyc = null; // инициализация неопред. знач.
-            Console.WriteLine(zyc);
+            //Console.WriteLine(zyc);
+            string qwe = String.Empty; // строка объявл. как пустая с помощью Empty
+            //Console.WriteLine(qwe);
+            string asd = "123"; // иницал. строковой переменной строкой литералом(константой)
+            var asdf = String.Empty; //строковое знач. переменной присваивавется без явного вызова конструктора 
+            string asdfg = new String('a', '6'); // иниц. строки с использованием конструктора класса String
+            char[] qwer = { 'm', 'e', 's', 's', 'a', 'g', 'e'};
+            string qwer1 = new string(qwer);
+            WriteLine(qwer1);
             Console.ReadKey();
         }
     }
